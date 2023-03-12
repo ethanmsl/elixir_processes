bae30ce081d000ee12238612c7973d2a6b684080
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-12 11:47:41 -0700

    tests: commented out example test

--- test/processes_test.exs
+++ test/processes_test.exs
@@ -2,7 +2,7 @@ defmodule ProcessesTest do
   use ExUnit.Case
   doctest Processes
 
-  test "greets the world" do
-    assert Processes.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Processes.hello() == :world
+  # end
 end
