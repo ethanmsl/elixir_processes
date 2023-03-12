759a4de2d4da87473078a4c485c2ecfbdb2b78ee
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-12 11:35:58 -0700

    feat: sending and receiving processes

--- lib/processes.ex
+++ lib/processes.ex
@@ -1,18 +1,60 @@
 defmodule Processes do
   @moduledoc """
-  Documentation for `Processes`.
+  just some play code to experiment with processes
+  (re: chapter 8 of the Elixir for Programmers 2 Coding Gnome course)
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Processes.hello()
-      :world
-
+  test to make sure module is working
   """
-  def hello do
-    :world
+  def test_func() do
+    IO.puts "Hi there, I'm working!"
   end
+
+  @doc """
+  receive messages and don't shut down
+  from `iex` call with:
+      `pid = spawn Processes, :receive_messages, []`
+  (simply calling it causes iex to seem to hang)
+  (assignign output tpu "pid" is for convenience, to 
+  make sending to it later easier)
+  """
+  def receive_messages() do
+    receive do
+      {:ok, msg} -> IO.puts "Success, received: #{msg}"
+      {:error, msg} -> IO.puts "Uh-oh, received: #{msg}"
+    end
+    receive_messages()
+  end
+
+  @doc """
+  receive messages and don't shut down
+  from `iex` call with:
+      `pid = spawn Processes, :receive_messages, []`
+  (simply calling it causes iex to seem to hang)
+  (assignign output tpu "pid" is for convenience, to 
+  make sending to it later easier)
+  """
+  def receive_messages_and_input(func_input) do
+    receive do
+      msg -> IO.puts "#{func_input}: #{msg}"
+    end
+    receive_messages()
+  end
+
+  @doc"""
+  send message to pid
+  takes boolean-ish whcih is translated to :ok/error
+  NOTE: use iex> `pid = v(?)` to get pid identifier to route give to this function 
+
+  Note: build in `send/2`
+  """
+  def send_b(pid, to_send, _is_okay = true) do
+    send pid ,{:ok, to_send}
+  end
+
+  def send_b(pid, to_send, _is_okay) do
+    send pid, {:error, to_send}
+  end
+
 end
